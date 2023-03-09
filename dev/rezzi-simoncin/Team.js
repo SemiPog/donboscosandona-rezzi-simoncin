5e97f5baaaa63823591ecbf2bcc60247eb8acba1
Eros Simoncin <e.simoncin@donboscosandona.it>
2023-03-09 11:01:01 +0100

    inizio

---
+++ dev/rezzi-simoncin/Team.js
@@ -0,0 +1,11 @@
+import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Container'
+import maxWidth from '@mui/system'
+
+export default function MyCarousel() {
+    <Grid>
+         <Container maxWidth={maxWidth}>
+            
+         </Container>
+    </Grid>
+}
