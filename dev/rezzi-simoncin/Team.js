9af58de33abf7563bbd2d2dffa2b620860445117
Eros Simoncin <e.simoncin@donboscosandona.it>
2023-03-10 13:18:40 +0100

    continuato

--- dev/rezzi-simoncin/Team.js
+++ dev/rezzi-simoncin/Team.js
@@ -2,10 +2,14 @@ import Grid from '@mui/material/Grid'
 import Container from '@mui/material/Container'
 import maxWidth from '@mui/system'
 
-export default function MyCarousel() {
+export default function MyCarousel(members,) {
     <Grid>
-         <Container maxWidth={maxWidth}>
-            
-         </Container>
+        <Container maxWidth={maxWidth}>
+            <div style={{ marginTop: '5px', color: '#494949' }}>
+
+                
+            </div>
+
+        </Container>
     </Grid>
 }
